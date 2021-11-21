5205bd621de307bd43ebb539e0cc78ab97da75a3
Jordi <jordismit20000@gmail.com>
2021-11-21 10:37:09 +0100

    setup

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
-#!/usr/bin/env python
 from setuptools import find_packages, setup
 
 setup(
     name="repocutter",
-    version="0.0",
+    version="0.0.0",
     description="Repocutter",
     author="Jordi Smit",
     author_email="jordismit2000@gmail.com",
     url="https://github.com/j0rd1smit/repocutter",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
+    license="MIT",
+    packages=["repocutter"],
     install_requires=[
         "prompt-toolkit>=3.0.22",
         "requests>=2.26.0",
     ],
+    zip_safe=False,
     entry_points={
         "console_scripts": [
             "repocutter=repocutter:main",
